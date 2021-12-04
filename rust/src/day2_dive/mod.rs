18a3a15d5e48f887cab09c665448a25ec7b3521c
Dobi, Jan Sándor <dobi.jan.sandor@gmail.com>
2021-12-04 17:46:14 +0100

    feat(rust): day2 task 2

--- rust/src/day2_dive/mod.rs
+++ rust/src/day2_dive/mod.rs
@@ -3,11 +3,12 @@ pub mod test;
 pub struct Position {
     x: u32,
     z: u32,
+    aim: u32,
 }
 
 impl Position {
     fn new() -> Self {
-        Self { x: 0, z: 0 }
+        Self { x: 0, z: 0, aim: 0 }
     }
 
     pub fn product(&self) -> u32 {
@@ -21,7 +22,7 @@ trait Command {
     }
 }
 
-// functions on position implement the command trait (~ functional interface)
+// ~functional interface, we can pass in closures where a Command would be expected
 impl<T> Command for T
 where
     T: Fn(Position) -> Position,
@@ -34,10 +35,11 @@ where
 struct CommandCreator {}
 
 impl CommandCreator {
-    fn create<'a>(command: &'a str) -> impl Fn(Position) -> Position + 'a {
+    // task 1
+    fn create(command: String) -> impl Fn(Position) -> Position {
         let (name, amount) = CommandCreator::parse_command(command);
         move |mut current_position| {
-            match name {
+            match name.as_str() {
                 "forward" => current_position.x = current_position.x + amount,
                 "up" => current_position.z = current_position.z - amount,
                 "down" => current_position.z = current_position.z + amount,
@@ -47,23 +49,52 @@ impl CommandCreator {
         }
     }
 
-    fn parse_command(command: &str) -> (&str, u32) {
+    // task 2
+    fn create_2(command: String) -> impl Fn(Position) -> Position {
+        let (name, amount) = CommandCreator::parse_command(command);
+        move |mut current_position| {
+            match name.as_str() {
+                "forward" => {
+                    current_position.x = current_position.x + amount;
+                    current_position.z = current_position.z + current_position.aim * amount;
+                }
+                "up" => current_position.aim = current_position.aim - amount,
+                "down" => current_position.aim = current_position.aim + amount,
+                _ => (),
+            };
+            current_position
+        }
+    }
+
+    fn parse_command(command: String) -> (String, u32) {
         command
             .split_once(' ')
+            .map(|(n, a)| (String::from(n), String::from(a)))
             .map(|(n, a)| a.parse::<u32>().ok().map(|parsed| (n, parsed)))
             .flatten()
-            .unwrap_or(("forward", 0)) // malformed commands are turned into no-ops
+            .unwrap_or(("forward".to_owned(), 0)) // malformed commands are turned into no-ops
     }
 }
 
 pub fn solve(path: &str) -> Position {
+    solve_with_creator(path, CommandCreator::create)
+}
+
+pub fn solve_2(path: &str) -> Position {
+    solve_with_creator(path, CommandCreator::create_2)
+}
+
+fn solve_with_creator<C>(path: &str, creator: impl Fn(String) -> C) -> Position
+where
+    C: Command,
+{
     use super::utils::read_lines;
 
     match read_lines(path) {
         None => Position::new(),
         Some(lines) => lines
-            .iter()
-            .map(|line| CommandCreator::create(line))
-            .fold(Position::new(), |pos, cmd| cmd(pos)),
+            .into_iter()
+            .map(creator)
+            .fold(Position::new(), |pos, cmd| cmd.run(pos)),
     }
 }
--- rust/src/day2_dive/test.rs
+++ rust/src/day2_dive/test.rs
@@ -3,9 +3,17 @@
 const PATH: &str = "puzzles/day2/input";
 
 #[test]
-pub fn day1_task1() {
+pub fn day2_task1() {
     println!(
         "[Day2 Task 1] Final position product: {}",
         super::solve(PATH).product()
+    )
+}
+
+#[test]
+pub fn day2_task2() {
+    println!(
+        "[Day2 Task 2] Final position product: {}",
+        super::solve_2(PATH).product()
     )
 }
